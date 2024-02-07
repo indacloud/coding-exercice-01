6f9f4909964291e3c4304370af0e834bd5256333
Tom Brehm <tom@indacloud.fr>
2024-02-07 12:38:42 +0100

    Dockerizing the app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  console.log({ props });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          What?????
+          Edit <code>src/App.js</code> and save to reload???
         </p>
         <a
           className="App-link"
